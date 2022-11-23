6b0c3097ac174c244e2560b8b7ee1780052140dc
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2022-11-24 01:07:23 +0700

    Bump to version 1.0

--- info.rkt
+++ info.rkt
@@ -5,6 +5,6 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/helpful.scrbl" ())))
 (define pkg-desc "Helpfully suggest a closest variable name on unbound identifier error.")
-(define version "0.0")
+(define version "1.0")
 (define pkg-authors '(sorawee))
 (define license '(Apache-2.0 OR MIT))
